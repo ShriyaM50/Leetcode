efe3417d9a5cc9bf9c57974ace5929a24a686ecf
Shriya Mittal <india.shriya.mittal0206@gmail.com>
2026-09-14 14:46:57 +0530

    Time: 15 ms (59.79%), Space: 51.4 MB (74.31%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,14 +1,14 @@
 class Solution {
     public int findContentChildren(int[] greed, int[] cookie) {
                 Arrays.sort(greed);
-        Arrays.sort(cookie);
+                Arrays.sort(cookie);
         int count=0;
        int i=0,j=0;
             while(i<greed.length&&j<cookie.length){
                 if(cookie[j]>=greed[i]){
                     count++;
                     i++;
-                    j++;
+                 j++;
                 
                 
             }
